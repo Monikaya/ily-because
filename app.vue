908ea6f9bf8350115971b101768b89bccee36a9a
ahawker374 <ahawker374@gmail.com>
2024-01-18 21:47:43 +0000

    added pages & triangle

--- app.vue
+++ app.vue
@@ -1,5 +1,3 @@
 <template>
-  <div>
-    <h1>penis</h1>
-  </div>
+  <NuxtPage />
 </template>
---
+++ pages/triangle.vue
@@ -0,0 +1,5 @@
+<template>
+    <div>
+        <img class="lame" src="https://wumbo.net/symbols/upside-down-triangle/feature.png">
+    </div>
+</template>
