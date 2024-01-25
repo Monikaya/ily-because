c8f66b28e67ea31454351eda00b5182daef18ec1
Monikaya <64984313+Monikaya@users.noreply.github.com>
2024-01-25 10:53:01 -0800

    cleaned up nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,14 +1,13 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: [
-    ['@nuxtjs/google-fonts', {
-      families: {
-        'Salsa': true,
-        download: true,
-        inject: true
-      }
-    }]
-  ],
+  modules: ['@nuxtjs/google-fonts'],
+  googleFonts: {
+    families: {
+      'Salsa': true,
+      download: true,
+      inject: true,
+    }
+  },
   css: ['~/assets/css/main.css', '~/assets/css/fonts.css'],
 })
