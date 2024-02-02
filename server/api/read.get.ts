e8aadbcb526f17e62c6664e0722113f44ba5e778
Monikaya <64984313+Monikaya@users.noreply.github.com>
2024-02-02 22:59:12 +0000

    can get data from the files!!!

--- server/api/read.get.ts
+++ server/api/read.get.ts
@@ -1,3 +1,8 @@
+import * as path from 'node:path';
+import * as fs from 'node:fs';
+
 export default defineEventHandler(async (event) => {
-  return 'Hello Nitro'
+  const filePath = 'public/reasons.txt';
+  const data = fs.promises.readFile(filePath, 'utf-8')
+  return data
 })
