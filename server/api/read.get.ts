c5078be7788d59e561a0e4bb9848152034f823fc
Monikaya <64984313+Monikaya@users.noreply.github.com>
2024-02-02 23:12:50 +0000

    got the file into an array w/ lines

--- server/api/read.get.ts
+++ server/api/read.get.ts
@@ -3,6 +3,8 @@ import * as fs from 'node:fs';
 
 export default defineEventHandler(async (event) => {
   const filePath = 'public/reasons.txt';
-  const data = fs.promises.readFile(filePath, 'utf-8')
-  return data
+  const list = await fs.promises.readFile(filePath, 'utf-8')
+  const thing = await list.split(/\r?\n|\r|\n/g);
+  
+  return thing
 })
