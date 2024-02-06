bc929baa99dcc31b93cdb52b29aa6314551166b7
Monikaya <64984313+Monikaya@users.noreply.github.com>
2024-02-06 19:26:38 +0000

    got it to return random string

--- server/api/read.get.ts
+++ server/api/read.get.ts
@@ -3,8 +3,11 @@ import * as fs from 'node:fs';
 
 export default defineEventHandler(async (event) => {
   const filePath = 'public/reasons.txt';
-  const list = await fs.promises.readFile(filePath, 'utf-8')
-  const thing = await list.split(/\r?\n|\r|\n/g);
-  
-  return thing
+  const blob = await fs.promises.readFile(filePath, 'utf-8');
+  const reasonArr = await blob.split(/\r?\n|\r|\n/g);
+
+  const randomDouble = (Math.random() * reasonArr.length);
+  const randomIndex = Math.floor(randomDouble);
+
+  return reasonArr[randomIndex];
 })
