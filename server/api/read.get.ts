f84b07235d608a57b8db53ac207a28613bf15e20
Monikaya <64984313+Monikaya@users.noreply.github.com>
2024-02-06 19:48:54 +0000

    fixed potential bug with cwd (prayge)

--- server/api/read.get.ts
+++ server/api/read.get.ts
@@ -1,7 +1,8 @@
+import * as path from 'node:path';
 import * as fs from 'node:fs';
 
 export default defineEventHandler(async (event) => {
-  const filePath = 'public/reasons.txt';
+  const filePath = path.join(process.cwd(), 'public', 'reasons.txt')
   const blob = await fs.promises.readFile(filePath, 'utf-8');
   const reasonArr = await blob.split(/\r?\n|\r|\n/g);
 
