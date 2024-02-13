924de0d221f9bdde5ba0bc2f35963214b067c7a1
Monikaya <64984313+Monikaya@users.noreply.github.com>
2024-02-13 06:57:55 +0000

    button works!!

---
+++ composables/useTypingEffect.js
@@ -0,0 +1,28 @@
+import { ref } from 'vue';
+
+export const useTypingEffect = (targetElement, typeSpeed = 100) => {
+  const displayString = ref('');
+  let index = 0;
+
+  const typeEffect = (text) => {
+    displayString.value = text.slice(0, index);
+    targetElement.value.textContent = displayString.value; // Update DOM
+    index++;
+
+    if (index > text.length) {
+      clearInterval(intervalId);
+    }
+  };
+
+  let intervalId;
+
+  const startEffect = (newText) => {
+    index = 0;
+    displayString.value = '';
+    clearInterval(intervalId);
+
+    intervalId = setInterval(() => typeEffect(newText), typeSpeed); 
+  };
+
+  return { displayString, startEffect };
+};
--- pages/index.vue
+++ pages/index.vue
@@ -1,14 +1,14 @@
 <template>
   <div>
-    <h1>I love you because:</h1>
-    <div @click="sillyReason()">
+    <h1>I love you because (of):</h1>
+    <div @click="typeNewReason()">
       <UButton variant="outline">
         Generate a Reason!
       </UButton>
     </div>
     <code>
       <div>
-        <p class="typing-effect">{{ reason }}</p>
+        <p ref="typingTarget"></p>
       </div>
     </code>
   </div>
@@ -16,12 +16,27 @@
 
 <script setup>
   import '~/assets/css/textEffect.css';
-  let reason = await sillyReason();
-</script>
+  import { ref } from 'vue';
+  import { useTypingEffect } from '~/composables/useTypingEffect';
 
-<script>
-  async function sillyReason() {
-    let reason = await useFetch('/api/read');
-    return reason.data;
-  }
+  let reason = await sillyReason();
+  const typingTarget = ref(null); 
+  const newString = ref(reason.value); // Initialize here 
+  const { displayString, startEffect } = useTypingEffect(typingTarget);
+  
+  onMounted(() => {
+    startEffect(newString.value);
+  });
+
+  async function typeNewReason() {
+      console.log("henlo");
+      let reason = await sillyReason();
+      newString.value = reason.value;
+
+      startEffect(newString.value);
+    }
+    async function sillyReason() {
+      let reason = await useFetch('/api/read');
+      return reason.data;
+    }
 </script>
